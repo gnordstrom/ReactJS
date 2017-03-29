c93d055c3742d62daf86b05b261bb00500dbce16
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-03-29 08:53:58 -0600

    Added work on redux sections

--- PluralSight-Corey-House/app1/src/components/course/CoursesPage.js
+++ PluralSight-Corey-House/app1/src/components/course/CoursesPage.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
 
 class CoursesPage extends React.Component {
     constructor(props, context) {
@@ -40,4 +41,6 @@ class CoursesPage extends React.Component {
     }
 }
 
-export default CoursesPage;
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
